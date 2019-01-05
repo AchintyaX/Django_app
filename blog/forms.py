ed9628b803db89968b6100a3dc6431ffcbab3246
AchintyaX <achintyashankhdhar@gmail.com>
2019-01-05 14:42:10 +0530

    comment model implemented

--- blog/forms.py
+++ blog/forms.py
@@ -10,4 +10,4 @@ class PostForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta :
         model = Comment
-        fields = ('author', 'text')
+        fields = ('author', 'text',)
