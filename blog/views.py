6b6ce05c3e1e176a6f9ad148eff194c3b1b319ef
AchintyaX <achintyashankhdhar@gmail.com>
2019-01-05 16:22:45 +0530

    fixed the bugs in the comment system

--- blog/views.py
+++ blog/views.py
@@ -59,25 +59,25 @@ def post_remove(request, pk):
 
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-            return redirect('post_detail', pk = post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/add_comment_to_post.html', {'form':form})
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
 @login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
-    return redirect('post_detail', pk = comment.post.pk)
+    return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
-    return redirect('post_detail', pk = comment.post.pk)
+    return redirect('post_detail', pk=comment.post.pk)
